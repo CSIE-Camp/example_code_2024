eca823a51c773c41dbc903dc6d0ab486f1660a5f
xinhui728 <75848380+xinhui728@users.noreply.github.com>
2024-06-20 11:54:45 +0800

    Update 7 game.py

--- Python_Basic/7 game.py
+++ Python_Basic/7 game.py
@@ -15,10 +15,20 @@ while True:
         line = input()  # string describing tiles of a line of the grid containing values: 0 or 1: player with this id, '-': empty, 'x': hole
         list.append(line.split(" "))
 
-    print(list, file=sys.stderr, flush=True)
-        
     # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)    
+    # print(list, file=sys.stderr, flush=True)
+
+    Weight = [0, 0, 0, 0] # UP, RIGHT, DOWN, LEFT
+
+    #todo
 
     
     # UP | RIGHT | DOWN | LEFT
+    max_i = 0
+    dir = ["UP", "RIGHT", "DOWN", "LEFT"]
+    for i in range(1, 4):
+        if Weight[i] > Weight[max_i]:
+            max_i = i
+    print(dir[max_i])
+  
