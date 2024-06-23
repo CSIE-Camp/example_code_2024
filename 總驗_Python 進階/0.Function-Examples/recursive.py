d98d2078f2a8e804c687dc80ce5188d93958b326
xinhui728 <75848380+xinhui728@users.noreply.github.com>
2024-06-24 00:50:52 +0800

    Delete 總驗_Python 進階 directory

--- 總驗_Python 進階/0.Function-Examples/recursive.py
+++
@@ -1,9 +0,0 @@
-def Fibonacci(n):
-    if n < 2:
-        return n
-    else:
-        return Fibonacci(n - 1) + Fibonacci(n - 2)
-
-print(Fibonacci(10))
-
-# https://zh.wikipedia.org/zh-tw/費波那契數
--- 總驗_Python 進階/3.Try_and_except-Examples/finally_example.py
+++
@@ -1,9 +0,0 @@
-try:
-    a = input()
-    a = int(a)
-except ValueError: # 如果有 ValueError
-    print("Value error")
-else: # 如果沒有錯誤
-    print("No error")
-finally: # 不管有沒有錯誤都會執行
-    print("Finally")
